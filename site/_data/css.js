185b6fca2eaf8a55b893067bed3ca7f53c40e4f6
Evan Rusmisel <evanrusmisel@gmail.com>
2020-05-04 20:22:55 -0500

    Imagine CSS compiler api

--- site/_data/css.js
+++ site/_data/css.js
@@ -1,19 +1,15 @@
 const path = require('path')
+const CSSCompiler = require('../../lib/css-compiler')
 
 const CSSInputDir = path.join(__dirname, '../_css')
 const CSSOutputDirHREF = '/css'
 const CSSOutputDir = path.join(__dirname, '../../public/', CSSOutputDirHREF)
 
-const postcssPlugins = [
-	require('postcss-preset-env'),
-	require('cssnano')
-]
+const compiler = new CSSCompiler()
+	.input(CSSInputDir)
+	.plugin(require('postcss-preset-env'), require('cssnano'))
+	.output(CSSOutputDir)
 
 module.exports = async () => {
-	const CSSFilePaths = glob(path.join(CSSInputDir, '**/*.css'))
-	const compiledCSSFiles = await Promise.all(CSSFilePaths.map(compile))
-	return compiledCSSFiles.reduce((obj, file) => {
-		obj[file.path] = file
-		return obj
-	}, {})
+	return await compiler.run();
 }
